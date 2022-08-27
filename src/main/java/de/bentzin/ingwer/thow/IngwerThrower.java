d6a4afa122fce0fefbc244fbd5fcd141bf56289d
TDR <tdr.minecraft@gmail.com>
2022-08-27 12:14:25 +0200

    Update IngwerThrower.java

--- src/main/java/de/bentzin/ingwer/thow/IngwerThrower.java
+++ src/main/java/de/bentzin/ingwer/thow/IngwerThrower.java
@@ -1,13 +1,43 @@
 package de.bentzin.ingwer.thow;
 
-public class IngwerThrower {
+import de.bentzin.ingwer.Ingwer;
+import de.bentzin.ingwer.logging.Logger;
+import de.bentzin.ingwer.logging.SystemLogger;
 
-    public static void accept(Throwable throwable) throws IngwerException {
+import java.sql.SQLException;
+
+public final class IngwerThrower {
+
+    public static IngwerThrower getInstance() {
+        return Ingwer.getIngwerThrower();
+    }
+
+    private Logger logger;
+            
+    public IngwerThrower() {
+        logger = Ingwer.getLogger().adopt("Thrower");
+    }
+    
+    
+
+    public  void accept(Throwable throwable) throws IngwerException {
         accept(throwable,ThrowType.GENERAL);
     }
 
-    public static void accept(Throwable throwable, ThrowType type) throws IngwerException {
+    public  void accept(Throwable throwable, ThrowType type) throws IngwerException {
         //TODO
+        if(throwable instanceof SQLException) {
+            
+        }
         throw new IngwerException(throwable, type);
+    }
+
+
+    public static void acceptS(Throwable throwable) throws IngwerException {
+        Ingwer.getIngwerThrower().accept(throwable);
+    }
+
+    public static void acceptS(Throwable throwable, ThrowType type) throws IngwerException {
+        Ingwer.getIngwerThrower().accept(throwable,type);
     }
 }
