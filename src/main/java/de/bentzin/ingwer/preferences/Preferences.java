e42f9af390e211f042d1a07540e120386674cff6
TDR <tdr.minecraft@gmail.com>
2022-08-27 12:15:07 +0200

    change layout and add logger

--- src/main/java/de/bentzin/ingwer/preferences/Preferences.java
+++ src/main/java/de/bentzin/ingwer/preferences/Preferences.java
@@ -1,12 +1,21 @@
 package de.bentzin.ingwer.preferences;
 
+import de.bentzin.ingwer.logging.Logger;
+import de.bentzin.ingwer.logging.SystemLogger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.UUID;
 
-public record Preferences(UUID superadmin, char prefix, StartType startType,@Nullable File custom_sqliteLocation) {
+public record Preferences(UUID superadmin, char prefix, StartType startType,
+                          @Nullable File custom_sqliteLocation,
+                          @NotNull Logger ingwerLogger
+
+
+
+
+                          ) {
 
 
     public boolean hasCustomSqliteLocation() {
@@ -18,6 +27,6 @@ public record Preferences(UUID superadmin, char prefix, StartType startType,@Nul
 
 
         return new Preferences(
-                superadmin,'+', startType,null);
+                superadmin,'+', startType,null, new SystemLogger("Ingwer"));
     }
 }
