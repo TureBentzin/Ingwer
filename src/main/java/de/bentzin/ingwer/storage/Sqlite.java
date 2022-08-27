333b59d128ed291408fc684eb7216c8a4d49e024
TDR <tdr.minecraft@gmail.com>
2022-08-27 12:15:12 +0200

    fix sql

--- src/main/java/de/bentzin/ingwer/storage/Sqlite.java
+++ src/main/java/de/bentzin/ingwer/storage/Sqlite.java
@@ -2,66 +2,72 @@ package de.bentzin.ingwer.storage;
 
 import de.bentzin.ingwer.Ingwer;
 import de.bentzin.ingwer.identity.Identity;
+import de.bentzin.ingwer.identity.permissions.IngwerPermission;
+import de.bentzin.ingwer.identity.permissions.IngwerPermissions;
+import de.bentzin.ingwer.logging.Logger;
+import de.bentzin.ingwer.logging.SystemLogger;
 import de.bentzin.ingwer.preferences.Preferences;
 import de.bentzin.ingwer.preferences.StartType;
-import de.bentzin.ingwer.thow.IngwerException;
 import de.bentzin.ingwer.thow.IngwerThrower;
 import de.bentzin.ingwer.thow.ThrowType;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.sql.Statement;
 
 public class Sqlite {
 
-    public static File getDefaultFile() throws URISyntaxException {
+    public Sqlite() throws URISyntaxException {
+        logger = Ingwer.getLogger().adopt("Storage");
+        db = getDefaultFile();
+    }
+
+
+    public  Logger logger;
+
+    public static @NotNull File getDefaultFile() throws URISyntaxException {
         File jar;
 
-        jar = new File(Ingwer.class.getProtectionDomain().getCodeSource().getLocation()
-                .toURI());
+        jar = new File(Ingwer.class.getProtectionDomain().getCodeSource().getLocation().toURI());
         File parent = jar.getParentFile();
+        File db;
         db = new File(parent,"data.sqlite");
         return db;
     }
 
-    private static File db;
-
-    static {
-        try {
-            db = getDefaultFile();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Connection connection;
 
 
-    public static void init() throws URISyntaxException, IOException {
+    private  File db;
+
+
+    private  Connection connection;
+
+
+    public  void init() throws URISyntaxException, IOException {
 
         System.out.println("db = " + db);
         db.createNewFile();
 
     }
 
-    public static boolean isConnected(){
+    public  boolean isConnected(){
         try {
-            return  connection.isValid(0);
+            return  !connection.isClosed();
         } catch (SQLException e) {
-            IngwerThrower.accept(e, ThrowType.STORAGE);
+            IngwerThrower.acceptS(e, ThrowType.STORAGE);
         }
         return false;
     }
 
-    private static void setupDB() throws SQLException {
+    private  void setupDB() throws SQLException {
         if(isConnected()) {
 
-            connection.createStatement().execute("create table if not exist identity\n" +
+            connection.createStatement().execute("create table if not exists identity\n" +
                     "(\n" +
                     "    user_id          integer     not null\n" +
                     "        constraint identity_pk\n" +
@@ -85,47 +91,84 @@ public class Sqlite {
             try {
                 throw new IllegalStateException("Database needs to be connected!");
             }catch (Exception e) {
-                IngwerThrower.accept(e, ThrowType.STORAGE);
+                IngwerThrower.acceptS(e, ThrowType.STORAGE);
             }
 
         }
     }
 
-    public static void connect() throws SQLException {
+    public  void connect() throws SQLException {
         connection = DriverManager.getConnection("jdbc:sqlite:" + db.getPath());
+        logger.debug("connection: " + connection.toString());
     }
 
-    public static void close(){
+    public  void close(){
         if(connection != null)
         try {
             connection.close();
         } catch (SQLException e) {
-            IngwerThrower.accept(e);
+            IngwerThrower.acceptS(e);
         }
     }
 
     public static void main(String[] args) {
+
         Ingwer.start(new Preferences(Identity.DEVELOPER_UUID,
                 '+',StartType.DEBUG_STANDALONE,
-                new File("E:\\WorkSpace\\Ich versuche es nochmal\\Ich versuche es nochmal\\WORKSPACE 2020\\Ingwer\\out\\artifacts\\ingwer_storage_jar\\data.sqlite")));
+                new File("E:\\WorkSpace\\Ich versuche es nochmal\\Ich versuche es nochmal\\WORKSPACE 2020\\Ingwer\\out\\artifacts\\ingwer_storage_jar\\data.sqlite"),
+                new SystemLogger("Ingwer")
+                ));
 
+        Sqlite storage = Ingwer.getStorage();
         try {
-            init();
-            connect();
-            setupDB();
+
+            storage.init();
+            storage.connect();
+            storage.setupDB();
+
+            //HARDCODE
+
+            Identity identity = new Identity("TEST", null, new IngwerPermissions(IngwerPermission.USE));
+            storage.saveIdentity(identity);
+
+            //<HARDCODE
         } catch (URISyntaxException | IOException e) {
-            IngwerThrower.accept(e);
+            IngwerThrower.acceptS(e);
         } catch (SQLException e) {
-            IngwerThrower.accept(e, ThrowType.STORAGE);
+            IngwerThrower.acceptS(e, ThrowType.STORAGE);
         }
     }
 
 
-    public static File getDb() {
+    public  File getDb() {
         return db;
     }
 
-    public static void setDb(File db) {
-        Sqlite.db = db;
+    public void setDb(File db) {
+        this.db = db;
     }
+
+
+    public  Identity saveIdentity(@NotNull Identity identity) {
+        try {
+            Statement statement = connection.createStatement();
+            if(
+            statement.execute(
+                    "INSERT INTO identity (user_name, player_uuid, user_permissions)\n" +
+                    "VALUES (" + identity.getName() + "," + identity.getUUID()
+                            + "," + identity.getCodedPermissions() + ")")){
+                logger.debug("successfully entered 1x identity!");
+            }else {
+                logger.error("failed to enter 1x identity!");
+            }
+
+
+        } catch (SQLException e) {
+            IngwerThrower.acceptS(e,ThrowType.STORAGE);
+        }
+
+        return identity;
+    }
+
+
 }
