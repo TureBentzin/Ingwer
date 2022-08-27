cee654ca3e82901699872dacefbeb17f0624c97e
TDR <tdr.minecraft@gmail.com>
2022-08-27 11:01:17 +0200

    update preferences

--- src/main/java/de/bentzin/ingwer/preferences/Preferences.java
+++ src/main/java/de/bentzin/ingwer/preferences/Preferences.java
@@ -1,18 +1,23 @@
 package de.bentzin.ingwer.preferences;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.UUID;
 
-public record Preferences(UUID superadmin, char prefix, StartType startType) {
+public record Preferences(UUID superadmin, char prefix, StartType startType,@Nullable File custom_sqliteLocation) {
 
 
+    public boolean hasCustomSqliteLocation() {
+        return custom_sqliteLocation != null;
+    }
 
     public static @NotNull Preferences getDefaults(
             UUID superadmin, StartType startType) {
 
 
         return new Preferences(
-                superadmin,'+', startType);
+                superadmin,'+', startType,null);
     }
 }
