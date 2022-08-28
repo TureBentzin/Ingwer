483a70b9c1beb93bd2e319636fb2f2db6cd13e84
TDR <tdr.minecraft@gmail.com>
2022-08-28 16:18:27 +0200

    Create StraightLineStringMessage.java

---
+++ src/main/java/de/bentzin/ingwer/message/StraightLineStringMessage.java
@@ -0,0 +1,27 @@
+package de.bentzin.ingwer.message;
+
+import de.bentzin.ingwer.Ingwer;
+import de.bentzin.ingwer.command.IngwerCommandSender;
+import de.bentzin.ingwer.utils.LoggingClass;
+import net.kyori.adventure.text.Component;
+import org.bukkit.command.CommandSender;
+
+public class StraightLineStringMessage implements IngwerMessage{
+
+    private String message;
+
+    public StraightLineStringMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void send(CommandSender sender) {
+        Component component = IngwerMessage.easyFormat(message);
+
+    }
+
+    @Override
+    public void send(IngwerCommandSender sender) {
+
+    }
+}
