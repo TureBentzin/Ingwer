e55374000f072906bfb00f01c747c79257b3cf8b
TDR <tdr.minecraft@gmail.com>
2022-08-28 16:18:23 +0200

    Update IngwerMessage.java

--- src/main/java/de/bentzin/ingwer/message/IngwerMessage.java
+++ src/main/java/de/bentzin/ingwer/message/IngwerMessage.java
@@ -1,7 +1,13 @@
 package de.bentzin.ingwer.message;
 
 import de.bentzin.ingwer.command.IngwerCommandSender;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 public interface IngwerMessage {
 
@@ -16,7 +22,30 @@ public interface IngwerMessage {
     <gray>--------------- [<light_purple>Ingwer<gray>]<gray> ---------------
      */
 
+     MiniMessage miniMessage = MiniMessage.miniMessage();
+     TextColor COLOR = NamedTextColor.GRAY;
+     TextColor ACCENT = NamedTextColor.BLUE;
+     Component INGWER = mm("<gray>[<light_purple>Ingwer<gray>]");
 
+     @Contract(pure = true)
+     static @NotNull Component addPrefix(Component message) {
+          Component component = INGWER;
+          component = component.append(message);
+          return component;
+     }
+
+
+     static @NotNull Component mm(String s) {
+          return miniMessage.deserialize(s);
+     }
+
+     static @NotNull Component colorize(String plain) {
+          return Component.text(plain).color(COLOR);
+     }
+
+     static @NotNull Component easyFormat(String plain) {
+          return addPrefix(colorize(plain));
+     }
     //message
 
 
