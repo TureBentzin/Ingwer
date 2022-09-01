2c54e00b8b0cf4e22324df7f82435ee8f492b490
TDR <tdr.minecraft@gmail.com>
2022-09-01 15:27:17 +0200

    add test command

--- src/main/java/de/bentzin/ingwer/command/internalcommands/MultipageCommand.java
+++ src/main/java/de/bentzin/ingwer/command/internalcommands/MultipageCommand.java
@@ -3,20 +3,28 @@ package de.bentzin.ingwer.command.internalcommands;
 import de.bentzin.ingwer.command.CommandTarget;
 import de.bentzin.ingwer.command.IngwerCommand;
 import de.bentzin.ingwer.command.IngwerCommandSender;
-import de.bentzin.ingwer.message.IngwerMessage;
-import net.kyori.adventure.text.Component;
+import de.bentzin.ingwer.message.*;
 import net.kyori.adventure.text.event.ClickEvent;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class MultipageCommand extends IngwerCommand {
     public MultipageCommand() {
         super("page", "for internal purposes");
+        for (int i = 0; i < 44; i++) {
+            oneLinedMessageList.add(new StraightLineStringMessage(i + ": Test         -  TEST"));
+        }
     }
+    private List<OneLinedMessage> oneLinedMessageList = new ArrayList<>();
 
     @Override
     public void execute(IngwerCommandSender commandSender, String[] cmd, CommandTarget senderType) {
 
-
-        IngwerMessage.easyFormat("hello!").clickEvent(ClickEvent.runCommand("hello"));
+        ArrayList<FramedMessage> generate;
+      //  generate = new FramedMultipageMessageGenerator(oneLinedMessageList).generate(10,
+       //         integer -> generate.get(integer).send(commandSender));
     }
 
     @Override
--- src/main/java/de/bentzin/ingwer/features/test/MulipageTestCommand.java
+++ src/main/java/de/bentzin/ingwer/features/test/MulipageTestCommand.java
@@ -3,19 +3,36 @@ package de.bentzin.ingwer.features.test;
 import de.bentzin.ingwer.command.CommandTarget;
 import de.bentzin.ingwer.command.IngwerCommand;
 import de.bentzin.ingwer.command.IngwerCommandSender;
+import de.bentzin.ingwer.identity.Identity;
+import de.bentzin.ingwer.message.FramedMessageKeeper;
+import de.bentzin.ingwer.message.MiniMessageMessage;
+import de.bentzin.ingwer.message.OneLinedMessage;
+import de.bentzin.ingwer.message.StraightLineStringMessage;
+
+import java.util.ArrayList;
 
 public class MulipageTestCommand extends IngwerCommand {
     public MulipageTestCommand() {
-        super("mulipage", "sends a multipage message");
+        super("pages", "sends a multipage message");
+        for (int i = 0; i < 44; i++) {
+            oneLinedMessages.add(new MiniMessageMessage("<gray>" +  i + ": Welcome to Ingwer!"));
+        }
     }
 
+    private ArrayList<OneLinedMessage> oneLinedMessages = new ArrayList<>();
     @Override
     public void execute(IngwerCommandSender commandSender, String[] cmd, CommandTarget senderType) {
+
+        if(commandSender instanceof Identity) {
+            Identity identity = (Identity) commandSender;
+            FramedMessageKeeper framedMessageKeeper = new FramedMessageKeeper(identity.getUUID(), oneLinedMessages, 8);
+            framedMessageKeeper.send(1);
+        }
 
     }
 
     @Override
     public CommandTarget[] getCommandTargets() {
-        return new CommandTarget[0];
+        return new CommandTarget[]{CommandTarget.INGAME};
     }
 }
