9f3b34ee2e21f3328e26051edd95948e234e6f5d
TDR <tdr.minecraft@gmail.com>
2022-09-01 15:26:46 +0200

    Create FramedMessageKeeper.java

---
+++ src/main/java/de/bentzin/ingwer/message/FramedMessageKeeper.java
@@ -0,0 +1,95 @@
+package de.bentzin.ingwer.message;
+
+import de.bentzin.ingwer.thow.IngwerThrower;
+import de.bentzin.ingwer.thow.ThrowType;
+import org.apache.logging.log4j.core.tools.picocli.CommandLine;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FramedMessageKeeper {
+
+    private final UUID recipient;
+    private final ArrayList<FramedMessage> messages;
+
+    public FramedMessageKeeper(UUID recipient, ArrayList<FramedMessage> messages) {
+        this.recipient = recipient;
+        this.messages = messages;
+    }
+
+    public FramedMessageKeeper(UUID recipient, List<OneLinedMessage> content, int pageLength) {
+        this.recipient = recipient;
+        this.messages = new FramedMultipageMessageGenerator(content).generate(pageLength, recipient, this::send);
+    }
+
+    protected boolean isPopulated() {
+        return messages != null && messages.size() != 0;
+    }
+
+    public void send(Integer page) {
+        if (isPopulated()) {
+
+            validatePage(page);
+            Player player = Bukkit.getPlayer(recipient);
+            if (player != null) {
+                messages.get(page -1).send(player); //execute!
+            } else {
+                try {
+                    throw new IllegalArgumentException("uuid is not associated with a player! >> " + recipient);
+                } catch (CommandLine.InitializationException e) {
+                    IngwerThrower.acceptS(e, ThrowType.MESSAGE);
+                }
+            }
+
+        } else {
+            try {
+                throw new CommandLine.InitializationException("messages is not ready yet!");
+            } catch (CommandLine.InitializationException e) {
+                IngwerThrower.acceptS(e, ThrowType.MESSAGE);
+            }
+        }
+    }
+
+    @Nullable
+    public MultilinedMessage getPage(Integer page) {
+        if (isPopulated()) {
+
+            validatePage(page);
+            Player player = Bukkit.getPlayer(recipient);
+
+            if (player != null) {
+                return messages.get(page); //execute!
+            } else {
+                try {
+                    throw new IllegalArgumentException("uuid is not associated with a player! >> " + recipient);
+                } catch (CommandLine.InitializationException e) {
+                    IngwerThrower.acceptS(e, ThrowType.MESSAGE);
+                }
+            }
+
+        } else {
+            try {
+                throw new CommandLine.InitializationException("messages is not ready yet!");
+            } catch (CommandLine.InitializationException e) {
+                IngwerThrower.acceptS(e, ThrowType.MESSAGE);
+            }
+        }
+        return null;
+    }
+
+    private void validatePage(Integer page) {
+        if (page > messages.size() || page <= 0) {
+            try {
+                throw new IllegalArgumentException("invalid page >> " + page + "/" + messages.size());
+            } catch (CommandLine.InitializationException e) {
+                IngwerThrower.acceptS(e, ThrowType.MESSAGE);
+            }
+        }
+
+
+    }
+}
