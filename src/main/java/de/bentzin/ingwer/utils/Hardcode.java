b27448e80587e9bb86dc6805455dd7b039351243
TDR <tdr.minecraft@gmail.com>
2022-09-02 14:55:03 +0200

    Create Hardcode.java

---
+++ src/main/java/de/bentzin/ingwer/utils/Hardcode.java
@@ -0,0 +1,13 @@
+package de.bentzin.ingwer.utils;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+@Documented
+/**
+ * Warns a developer about hardcore
+ */
+public @interface Hardcode {
+}
