14dd3544c8d235691c9ad626c09b01920f754184
TDR <tdr.minecraft@gmail.com>
2022-09-03 09:35:23 +0200

    Update Hardcode.java

--- src/main/java/de/bentzin/ingwer/utils/Hardcode.java
+++ src/main/java/de/bentzin/ingwer/utils/Hardcode.java
@@ -7,6 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.SOURCE)
 @Documented
 /**
+ * @author Ture Bentzin
  * Warns a developer about hardcore
  */
 public @interface Hardcode {
