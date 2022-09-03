cffd4f6df16929a4d1a73b41eebfc1b9bb528447
TDR <tdr.minecraft@gmail.com>
2022-09-03 11:10:49 +0200

    add CollectionUtils

---
+++ src/main/java/de/bentzin/ingwer/utils/CollectionUtils.java
@@ -0,0 +1,22 @@
+package de.bentzin.ingwer.utils;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionUtils {
+
+    @Independent
+    @Contract("_, _ -> param1")
+    public static <E,C extends Collection<E>> @NotNull C flipFlop(@NotNull C collection, E element) {
+        if(collection.contains(element)) {
+            collection.remove(element);
+        }else {
+            collection.add(element);
+        }
+        return collection;
+    }
+    
+}
