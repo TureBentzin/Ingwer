d3b2ddb9e1c6cb0045f4649a43ea01ffca1af618
TDR <tdr.minecraft@gmail.com>
2022-09-03 11:11:10 +0200

    add experimental features

--- src/main/java/de/bentzin/ingwer/command/IngwerCommand.java
+++ src/main/java/de/bentzin/ingwer/command/IngwerCommand.java
@@ -1,16 +1,23 @@
 package de.bentzin.ingwer.command;
 
 import de.bentzin.ingwer.Ingwer;
+import de.bentzin.ingwer.identity.Identity;
 import de.bentzin.ingwer.logging.Logger;
+import de.bentzin.ingwer.message.StraightLineStringMessage;
 import de.bentzin.ingwer.thow.ThrowType;
 import de.bentzin.tools.register.Registerator;
-import org.bukkit.command.Command;
+import it.unimi.dsi.fastutil.Pair;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public abstract class IngwerCommand {
 
@@ -77,5 +84,45 @@ public abstract class IngwerCommand {
 
     public abstract CommandTarget[] getCommandTargets();
 
+
+    @ApiStatus.Experimental
+    public <T extends IngwerCommand> Identity identityCommand(IngwerCommandSender commandSender, @NotNull CommandTarget senderType, Consumer<Identity> action) {
+        if(senderType.equals(CommandTarget.INGAME)) {
+            if (commandSender instanceof Identity) {
+                Identity identity = (Identity) commandSender;
+                action.accept(identity);
+                return identity;
+            }
+        }
+        return null;
+    }
+
+
+    @ApiStatus.Experimental
+    @NotNull
+    public <T extends IngwerCommand> Pair<@Nullable Identity, @Nullable Player> identityPlayerCommand(IngwerCommandSender commandSender, @NotNull CommandTarget senderType, String[] cmd, BiConsumer<Identity, Player> action) {
+        StraightLineStringMessage specify_online_player = new StraightLineStringMessage("Please specify an online Player!");
+        if(senderType.equals(CommandTarget.INGAME)) {
+            if (commandSender instanceof Identity) {
+                Identity identity = (Identity) commandSender;
+                if (cmd.length == 1) {
+                    specify_online_player.send(identity);
+                } else if (cmd.length > 2) {
+
+                } else if (cmd.length == 2) {
+                    String s = cmd[1];
+                    Player player = Bukkit.getPlayer(s);
+                    if (player != null) {
+                        action.accept(identity,player);
+                        return Pair.of(identity,player);
+                    }else {
+                        specify_online_player.send(identity);
+                    }
+                }
+                return Pair.of(identity,null);
+            }
+        }
+        return Pair.of(null,null);
+    }
 
 }
