288b68ce8e9c8879d9877d151cbe4e67c4ab535b
TDR <tdr.minecraft@gmail.com>
2022-09-22 15:59:25 +0200

    modify exceptions

--- src/main/java/de/bentzin/ingwer/thow/IngwerException.java
+++ src/main/java/de/bentzin/ingwer/thow/IngwerException.java
@@ -6,23 +6,29 @@ public class IngwerException extends RuntimeException {
 
     private final ThrowType type;
 
+    private final String customMessage;
+
     public IngwerException(ThrowType type) {
         super();
+        customMessage = null;
         this.type = type;
     }
 
     public IngwerException(String message, ThrowType type) {
         super(message);
+        customMessage = message;
         this.type = type;
     }
 
     public IngwerException(String message, Throwable cause, ThrowType type) {
         super(message, cause);
+        customMessage = message;
         this.type = type;
     }
 
     public IngwerException(Throwable cause, ThrowType type) {
-        super("Error executing Ingwer v. " + Ingwer.VERSION_STRING, cause);
+        super(null, cause);
+        customMessage = null;
         this.type = type;
     }
 
@@ -34,6 +40,9 @@ public class IngwerException extends RuntimeException {
 
     @Override
     public String getMessage() {
+        if(customMessage != null) {
+            return type.getMessage() + " :: " + customMessage;
+        }
         return type.getMessage();
     }
 }
