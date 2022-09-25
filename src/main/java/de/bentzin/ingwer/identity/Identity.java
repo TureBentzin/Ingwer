b204982cb2ab713946683801911998d8e874b57b
Ture Bentzin <38729194+TureBentzin@users.noreply.github.com>
2022-09-25 11:56:00 +0200

    fix issue

--- src/main/java/de/bentzin/ingwer/identity/Identity.java
+++ src/main/java/de/bentzin/ingwer/identity/Identity.java
@@ -18,6 +18,7 @@ import java.util.UUID;
 
 public class Identity implements IngwerCommandSender {
 
+
     public static final UUID DEVELOPER_UUID = UUID.fromString("be6e2c93-694b-4cdf-827f-83d6f2d42fb9");
 
     public static final Set<Identity> IDENTITY_SET = new HashSet<>();
