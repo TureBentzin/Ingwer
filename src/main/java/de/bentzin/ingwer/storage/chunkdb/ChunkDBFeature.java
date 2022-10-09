a11498cb399ea48262c7217b9e5ec073ef136689
TDR <tdr.minecraft@gmail.com>
2022-10-09 13:04:27 +0200

    Create ChunkDBFeature.java

---
+++ src/main/java/de/bentzin/ingwer/storage/chunkdb/ChunkDBFeature.java
@@ -0,0 +1,57 @@
+package de.bentzin.ingwer.storage.chunkdb;
+
+import de.bentzin.ingwer.Ingwer;
+import de.bentzin.ingwer.command.CommandTarget;
+import de.bentzin.ingwer.command.node.IngwerNodeCommand;
+import de.bentzin.ingwer.command.node.LambdaAgrumentNode;
+import de.bentzin.ingwer.features.SimpleFeature;
+import de.bentzin.ingwer.identity.permissions.IngwerPermission;
+import de.bentzin.ingwer.message.builder.C;
+import de.bentzin.ingwer.message.builder.MessageBuilder;
+
+/**
+ * @author Ture Bentzin
+ * 09.10.2022
+ */
+class ChunkDBFeature extends SimpleFeature {
+    private final ChunkDB chunkDB;
+
+    public ChunkDBFeature(ChunkDB chunkDB) {
+        super("ChunkDB","Feature assisting ChunkDB");
+        this.chunkDB = chunkDB;
+    }
+
+    @Override
+    public IngwerPermission generalUsePermission() {
+        return IngwerPermission.ADMIN;
+    }
+
+    @Override
+    public void onEnable() {
+        new ChunkDBCommand(chunkDB);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public boolean onLoad() {
+        return Ingwer.getStorage() instanceof ChunkDB;
+    }
+
+    public static class ChunkDBCommand extends IngwerNodeCommand{
+        public ChunkDBCommand(ChunkDB chunkDB) {
+            super(new CommandTarget[]{CommandTarget.SAVE}, "chunkDb", "manage chunkDB", (data, nodeTrace) -> {
+                chunkDB.getStatusMessage().send(data.commandSender());
+            });
+
+            getCommandNode().append(new LambdaAgrumentNode("clear",(data, nodeTrace) -> {
+                chunkDB.clean();
+                MessageBuilder.prefixed().add(C.C,"ChunkDB wurde gelöscht!");
+            }));
+
+        }
+    }
+}
