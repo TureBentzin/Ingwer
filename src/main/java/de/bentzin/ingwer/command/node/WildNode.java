a97d6590a46092000ef91bc0c21261c387c7f9a9
TDR <tdr.minecraft@gmail.com>
2022-10-11 13:52:54 +0200

    Create WildNode.java

---
+++ src/main/java/de/bentzin/ingwer/command/node/WildNode.java
@@ -0,0 +1,60 @@
+package de.bentzin.ingwer.command.node;
+
+import de.bentzin.ingwer.utils.CompletableOptional;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * @author Ture Bentzin
+ * 11.10.2022
+ */
+public abstract class WildNode<T> extends AbstractNode<T> {
+
+    private final Predicate<String> condition;
+    private final CompletableOptional<CommandNode> commandNode = new CompletableOptional<>();
+
+    public WildNode(String name, Predicate<String> condition) {
+        super(name);
+        this.condition = condition;
+    }
+
+    public WildNode(String name, ArrayList<Node> nodes, Predicate<String> condition) {
+        super(name, nodes);
+        this.condition = condition;
+    }
+
+    @Override
+    public Collection<String> values() {
+        return List.of();
+    }
+
+    @Override
+    public final boolean resembles(String value) {
+        boolean test = condition.test(value);
+        getCommandNode().getOrThrow().getLogger().debug("testing: " + value + " --> " + test);
+        return test;
+    }
+
+    @Override
+    public void initialize(CommandNode commandNode) {
+        this.commandNode.complete(commandNode);
+    }
+
+    @Override
+    public CompletableOptional<CommandNode> getCommandNode() {
+        return commandNode;
+    }
+
+    @Override
+    public boolean uncertainValues() {
+        return true;
+    }
+
+    @Override
+    public boolean singleValued() {
+        return false;
+    }
+}
