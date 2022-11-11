87d3d9501b4f245e9ea4c12cc882c2b82ddb48a1
TDR <tdr.minecraft@gmail.com>
2022-11-11 08:46:56 +0100

    bump version to 0.6-BETA

--- src/main/java/de/bentzin/ingwer/Ingwer.java
+++ src/main/java/de/bentzin/ingwer/Ingwer.java
@@ -35,7 +35,7 @@ import java.util.function.Consumer;
 public final class Ingwer {
 
     //TODO dynamic
-    public static final String VERSION_STRING = "0.5-BETA";
+    public static final String VERSION_STRING = "0.6-BETA";
     public static final String BANNER = "\n" +
             "██╗███╗░░██╗░██████╗░░██╗░░░░░░░██╗███████╗██████╗░\n" +
             "██║████╗░██║██╔════╝░░██║░░██╗░░██║██╔════╝██╔══██╗\n" +
